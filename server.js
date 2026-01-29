a9072dc7babba7cc2bd6d5e9cfba0eaf13e7fae7
Graphics <Graphics@datascienceirc.com>
2026-01-29 16:14:54 +0530

    server file updated

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
+
 
 db.connect(err => {
   if (err) console.log("MySQL error:", err);
